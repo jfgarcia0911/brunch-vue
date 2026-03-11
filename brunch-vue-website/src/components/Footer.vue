9f1a9caa4b38290b6cbcb92e45f2d6a7f8e35822
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-11 14:53:07 +0800

    feat: add initial About section template
    - Introduced testimonial and About section layout
    - Included social media icons and placeholder content
    - Structured About description and link columns

---
+++ brunch-vue-website/src/components/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <!-- Main section wrapper -->
+  <section class="bg-darkgray h-165">
+    <!-- Top dark section with testimonial -->
+    <div class="text-white h-85">
+      <div class="items-center justify-center flex flex-col">
+        <p class="text-[83.33px] font-bold -mt-9">,,</p>
+        <p class="text-[18.75px] mt-3 mb-6 w-180 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        </p>
+        <h1 class="font-bold text-[18.75px]">Linda Sterward</h1>
+        <div class="flex gap-9 mt-6">
+          <img src="/facebook.png" alt="Facebook Icon" />
+          <img src="/twitter.png" alt="Facebook Icon" />
+          <img src="/instagram.png" alt="Facebook Icon" />
+          <img src="/pinterest.png" alt="Facebook Icon" />
+          <img src="/google.png" alt="Facebook Icon" />
+        </div>
+      </div>
+    </div>
+    <!-- Bottom light section with About info -->
+    <div class="bg-light h-80 ">
+      <div class="flex  justify-center text-darkgray ">
+         <div class="flex justify-between w-270">
+          <!-- About description -->
+        <div class="mt-17">
+          <h1 class="font-bold text-[25px] mb-12">ABOUT</h1>
+          <p class="w-79 text-[18.75px]">
+            Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eisumod temopor
+            incididunt ultiman quantum
+          </p>
+        </div>
+         <!-- About links (3 columns) -->
+        <div class="flex gap-29 mt-17 ">
+          <div class="font-bold text-[20.83px] ">
+            <h1 class="mb-12">ABOUT</h1>
+            <div>
+              <p>Team</p>
+              <p>Join us</p>
+              <p>Ethic</p>
+              <p>Goals</p>
+            </div>
+          </div>
+          <div class="font-bold text-[20.83px]">
+            <h1 class="mb-12">ABOUT</h1>
+            <div>
+              <p>Team</p>
+              <p>Join us</p>
+              <p>Ethic</p>
+              <p>Goals</p>
+            </div>
+          </div>
+          <div class="text-[20.83px]">
+            <h1 class="font-bold mb-12">ABOUT</h1>
+            <div>
+              <p>Team</p>
+              <p>Join us</p>
+              <p>Ethic</p>
+              <p>Goals</p>
+            </div>
+          </div>
+        </div>
+         </div>
+      </div>
+    </div>
+  </section>
+</template>
