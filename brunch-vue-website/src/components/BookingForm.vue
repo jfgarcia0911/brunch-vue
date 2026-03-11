4a7a4907c92f7928f36ba43b6a6d868e208d04da
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-11 14:50:58 +0800

    refactor: improve booking form layout and responsiveness

--- brunch-vue-website/src/components/BookingForm.vue
+++ brunch-vue-website/src/components/BookingForm.vue
@@ -4,39 +4,35 @@ export default {
     return {
       items: [
         {
-          image: "/polygon.png",
+          image: '/polygon.png',
           title: 'How many people?',
-          subTitle: "2 people",
+          subTitle: '2 people',
         },
         {
-          image: "/calendar.png",
+          image: '/calendar.png',
           title: 'When is your?',
-          subTitle: "17-02-2020",
+          subTitle: '17-02-2020',
         },
         {
-          image: "/clock.png",
+          image: '/clock.png',
           title: 'Time',
-          subTitle: "10:00am",
+          subTitle: '10:00am',
         },
         {
-          image: "",
+          image: '',
           title: 'Name',
-          subTitle: "Full name",
+          subTitle: 'Full name',
         },
         {
-          image: "",
+          image: '',
           title: 'Email',
-          subTitle: "Your email address",
+          subTitle: 'Your email address',
         },
         {
-          image: "",
+          image: '',
           title: 'Phone',
-          subTitle: "Your phone",
+          subTitle: 'Your phone',
         },
-
-
-
-
       ],
     }
   },
@@ -49,24 +45,36 @@ export default {
       :style="{ backgroundImage: `url(/booking-form-bg.png)` }"
       class="flex bg-cover bg-center h-220 w-full"
     ></div>
-    <div class="h-112 bg-darkgray text-white">
-      <div>
-        <div class="flex justify-center">
-          <h1 class="font-[Cookie] text-[77.11px] text-center mt-5">Book here</h1>
+    <div class="h-112 bg-darkgray w-full  text-white">
+      <div class="w-292 justify-center mx-auto">
+        <div>
+          <div class="flex justify-center">
+            <h1 class="font-[Cookie] text-[77.11px] text-center mt-5">Book here</h1>
+          </div>
         </div>
-      </div>
-      <div class="flex flex-wrap justify-center">
-        <div v-for="(item,index) in items" :key="index" class="font-bold text-[16.67px] w-1/3 mb-9 flex justify-center">
+        <div class="flex flex-wrap justify-center">
+          <div
+            v-for="(item, index) in items"
+            :key="index"
+            class="font-bold text-[16.67px] md:w-1/2 lg:w-1/3 mb-9 flex justify-center"
+          >
+            <div>
+              <h1>{{ item.title }}</h1>
+              <div class="border rounded-lg flex items-center justify-between px-2 mt-1 w-60">
+                <h2 class="">{{ item.subTitle }}</h2>
+                <img v-if="item.image" :src="item.image" alt="Polygon" />
+              </div>
+            </div>
+          </div>
           <div>
-            <h1>{{item.title}}</h1>
-          <div class="border rounded-lg flex items-center justify-between px-2 mt-1 w-60">
-            <h2 class="">{{item.subTitle}}</h2>
-            <img v-if="item.image" :src="item.image" alt="Polygon">
-          </div>
+            <button class="border w-60 rounded-lg font-bold text-[16.67px] mt-4">SUBMIT</button>
           </div>
         </div>
-        <button class="border  w-60 rounded-lg font-bold text-[16.67px] mt-4">SUBMIT</button>
       </div>
     </div>
+    <div
+      :style="{ backgroundImage: `url(/booking-form-bg2.png)` }"
+      class="flex bg-cover bg-center h-216 w-full"
+    ></div>
   </section>
 </template>
