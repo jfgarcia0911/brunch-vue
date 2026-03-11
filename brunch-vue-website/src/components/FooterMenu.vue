6e46556c58d1a06359dfc0a1a6b2966cc20f4ad9
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-03-11 23:00:28 +0800

    feat: add FooterMenu component with Discover section
    - Implemented footer menu layout with "Discover" and "Our New Menu" headings
    - Added card grid rendering items from data array (image, title, description, date)
    - Styled cards with responsive flexbox, spacing, and button
    - Included placeholder content and images for food, spices, and chicken curry

---
+++ brunch-vue-website/src/components/FooterMenu.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          image: '/food-footer.png',
+          title: 'Lorem ipsum dolor sit',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed',
+          date: 'Food / 16th March, 2019',
+        },
+        {
+          image: '/spices-footer.png',
+          title: 'Lorem ipsum dolor sit',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed',
+          date: 'Spices / 16th March, 2019',
+        },
+        {
+          image: '/chicken-curry-footer.png',
+          title: 'Lorem ipsum dolor sit',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed',
+          date: 'Chicken Curry / 16th March, 2019',
+        },
+      ],
+    }
+  },
+}
+</script>
+<template>
+  <section class="h-204 bg-darkgray">
+    <div class="h-204 bg-light ">
+      <div class="">
+        <div class="flex justify-center flex-col items-center">
+          <h1 class="font-['Cookie'] text-[77.11px] text-darkgray mt-4">Discover</h1>
+          <p class="font-semibold text-[47.59px] text-[#996600] -mt-11">OUR NEW MENU</p>
+        </div>
+        <div class="mt-10">
+          <div class="flex flex-wrap items-center justify-center w-full gap-8">
+            <!-- Card -->
+            <div v-for="(item, index) in items" :key="index" class="w-85 h-124 bg-white">
+              <div>
+                <img :src="item.image" alt="Food Menu" />
+              </div>
+              <div class="m-6 text-darkgray">
+                <h2 class="font-bold text-[14px]">Food / 16th March, 2019</h2>
+                <div class="my-3">
+                  <h1 class="font-bold text-[16.67px]">Lorem ipsum dolor sit</h1>
+                  <p class="text-[16.67px]">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  </p>
+                </div>
+                <button class="font-bold text-[14px]">READ MORE</button>
+                <div class="h-2 w-20 bg-[#996600]"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
